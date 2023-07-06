453a6708f81ba699968cc6b0acc5e4d2b32936e9
nikola chakarov <chakarovn1975@gmail.com>
2023-07-06 22:12:48 +0300

    some state

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,4 +1,9 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 2, description: 'Socks', quantity: 12, packed: true },
+	{ id: 3, description: 'Charger', quantity: 1, packed: false },
+];
 
 function App() {
 	return (
@@ -20,15 +25,69 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescription] = useState('');
+	const [quantity, setQuantity] = useState(1);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		if (!description) return;
+
+		const newItem = {
+			description,
+			quantity,
+			packed: false,
+			id: Date.now(),
+		};
+		console.log(newItem);
+
+		setDescription((prev) => '');
+		setQuantity((prev) => 1);
+	}
+
 	return (
-		<div className='add-form'>
+		<form className='add-form' onSubmit={handleSubmit}>
 			<h3>What do you need for your 😎 trip?</h3>
-		</div>
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity((prev) => Number(e.target.value))}
+			>
+				<option value={1}>one</option>
+				<option value={2}>two</option>
+				<option value={3}>three</option>
+			</select>
+			<input
+				type='text'
+				placeholder='Item...'
+				value={description}
+				onChange={(e) => setDescription((prev) => e.target.value)}
+			/>
+			<button type='submit'>Add</button>
+		</form>
 	);
 }
 
 function PackingList() {
-	return <div className='list'>LIST</div>;
+	return (
+		<div className='list'>
+			<ul>
+				{initialItems.map((item, idx) => (
+					<Item key={idx} {...item} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({ description, quantity, packed }) {
+	return (
+		<li>
+			<span style={packed ? { textDecoration: 'line-through' } : {}}>
+				{description} {quantity}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats() {
